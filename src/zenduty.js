731f4a7a42e691ff4f761e1e591ecde4edc11132
windynik <nikhilprem@rocketmail.com>
2019-12-18 13:36:26 +0530

    Remove unwanted code / add help command

--- src/zenduty.js
+++ src/zenduty.js
@@ -1,5 +1,3 @@
-// #   These are from the scripting documentation: https://github.com/github/hubot/blob/master/docs/scripting.md
-
 zenDutyApiKey = process.env.HUBOT_ZENDUTY_API_KEY;
 
 if (zenDutyApiKey == null) {
@@ -11,17 +9,7 @@ if (zenDutyApiKey == null) {
 zenduty = require("zenduty-sdk");
 
 module.exports = function(robot) {
-  robot.respond(/is it (weekend|holiday)\s?\?/i, function(msg) {
-    var today = new Date();
-
-    msg.send(today.getDay() === 0 || today.getDay() === 6 ? "YES" : "NO");
-  });
-
-  // robot.hear(/badger/i, function(res) {
-  //   res.send("Badgers? BADGERS? WE DON'T NEED NO STINKIN BADGERS");
-  // });
-
-  robot.respond(/get (.*)/i, function(res) {
+  robot.respond(/zen get (.*)/i, function(res) {
     incidentNumber = res.match[1];
     res.send(
       `Getting you information on incident number #${incidentNumber}, gimme a second...`
@@ -42,7 +30,6 @@ module.exports = function(robot) {
           `Incident Number #${incidentNumber}\nTitle : ${response.title}\nSummary : ${response.summary}\nAssigned To :  ${response.assigned_to_name}\nStatus : ${status}
         `
         );
-        // res.send(JSON.stringify(response));
       })
       .catch(function(e) {
         console.log(e);
@@ -52,7 +39,7 @@ module.exports = function(robot) {
       });
   });
 
-  robot.respond(/ack (.*)/i, function(res) {
+  robot.respond(/zen ack (.*)/i, function(res) {
     incidentNumber = res.match[1];
     res.send(
       `Give me a second to Acknowledge incident number #${incidentNumber} ...`
@@ -72,7 +59,7 @@ module.exports = function(robot) {
         res.send("There seems to be some problem..\nDo contact my maker!");
       });
   });
-  robot.respond(/res (.*)/i, function(res) {
+  robot.respond(/zen res (.*)/i, function(res) {
     incidentNumber = res.match[1];
     res.send(
       `Give me a second to Resolve incident number #${incidentNumber} ...`
@@ -93,37 +80,7 @@ module.exports = function(robot) {
       });
   });
 
-  robot.respond(/res (.*)/i, function(res) {
-    incidentNumber = res.match[1];
-    res.send(
-      `Give me a second to Resolve incident number #${incidentNumber} ...`
-    );
-    api_obj = new zenduty.IncidentsApi(new zenduty.ApiClient(zenDutyApiKey));
-    payload = {
-      status: 3,
-      incident_number: incidentNumber
-    };
-    api_obj
-      .acknowledgeOrResolveIncident(incidentNumber, payload)
-      .then(function(response) {
-        res.send(`Ok, the incident has been Resolved.`);
-      })
-      .catch(function(e) {
-        console.log(e);
-        res.send("There seems to be some problem..\nDo contact my maker!");
-      });
-  });
-
-  robot.respond(/incidents (.*)/i, function(res) {
-    // if (res.match[1] == null) {
-    //   res.send(
-    //     `I would have retrieved the incidents for you, but you didn't mention any page numbers. \n(10 incidents per page.)`
-    //   );
-    //   res.send(`I don't want to flood you with walls of text, Soo`);
-    //   res.send(`Just use (<My Name> incidents <Page Number>), Thank you! :)`);
-    // } else {
-    //   pageNumber = res.match[1];
-    // }
+  robot.respond(/zen incidents (.*)/i, function(res) {
     pageNumber = res.match[1];
     res.send(`Retriving all incidents for you..`);
     api_obj = new zenduty.IncidentsApi(new zenduty.ApiClient(zenDutyApiKey));
@@ -154,7 +111,23 @@ module.exports = function(robot) {
       });
   });
 
-  // robot.respond(/incidents/i, function(res) {
+  robot.respond(/zen help/i, function(res) {
+    res.send(`Currently, you can use the following commands with Zen : `);
+    res.send(
+      `hubot get <Incident Number> : To get a specific incident's details.`
+    );
+    res.send(
+      `hubot ack <Incident Number> : To Acknowledge a specific incident.`
+    );
+    res.send(`hubot res <Incident Number> : To Resolve a specific incident.`);
+    res.send(
+      `hubot incidents <Page Number> : To get all incidents from a specific page \n( 1 being the latest set of incidents ).`
+    );
+    res.send(`\n\nContact my makers to request for more commands`);
+    res.send(`Happy Zenning! :)`);
+  });
+
+  // robot.respond(/zen incidents/i, function(res) {
   // res.send(
   //   `I would have retrieved the incidents for you, but you didn't mention any page numbers. \n(10 incidents per page.)`
   // );
